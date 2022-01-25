cf090842301d7d091e972f377568086a05f9439d
Austin Rolert <austinrolert@gmail.com>
2022-01-25 15:25:19 +0630

    Create begin.dart

---
+++ begin.dart
@@ -0,0 +1,41 @@
+// void main() {
+//  String name_of_person = "Austin";
+//  int age = 16;
+//  double person_brithday = 12.41;
+//  bool = false
+ 
+//   print("The boy name is ${name_of_person} and he is ${age} years old");
+// }
+
+// void main() {
+// 	String name ="Austin";
+
+// 	print(name.toUpperCase()
+// 	);
+// }
+
+// void main() {
+// 	String name = "Austin";
+// 	print(name.indexOf("t"));
+// }
+
+// // the output is 3
+
+// void main() 
+// 	String name = "Austin";
+// 	print(name.indexOf("t"));
+
+void main() {
+  print("idk what the fuck is this");
+}
+// }
+
+// // the output is 3
+// I don't know you would hate me that much 
+void name() {
+  print(3 + 1);
+}
+
+age() {
+  print(4);
+}
